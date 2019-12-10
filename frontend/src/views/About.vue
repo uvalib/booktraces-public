f8f7bbaeda5800a9f6be8417172af433e4bad9af
Lou Foster <lf6f@virginia.edu>
2019-12-10 15:38:00 -0500

    update deps. new about text

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,36 +1,55 @@
 <template>
    <div class="about content">
       <h2>About</h2>
+      <dl>
+         <dt>Director</dt>
+         <dd>
+            <a href="http://www.engl.virginia.edu/people/ams4k" target="_blank">Andrew Stauffer</a>,&nbsp;
+            Department of English, University of Virginia
+         </dd>
+         <dt>Project Manager</dt>
+         <dd>
+            <a href="https://www.library.virginia.edu/staff/khj5c/" target="_blank">Kristin Jensen</a>,&nbsp;
+            University of Virginia Library
+         </dd>
+      </dl>
       <p>
-         “…some hand, now forever quiet, had made at certain passages strong pen and ink marks,
-         long since browned by time.” — George Eliot, The Mill on the Floss (1860)
+         <router-link to="/">Book Traces</router-link> is a collaborative web project aimed at discovering, cataloging, and preserving
+         unique copies of
+         <b>nineteenth- and early twentieth-century books on library shelves</b>.
+         Our focus is on
+         <b>circulating books</b> (not special collections) that were customized by their original owners, who left in them
+         marginalia, annotations, inscriptions, and insertions. At least until the 1950s, academic libraries in particular built their
+         collections from donations, collecting secondhand books from alumni, local families, faculty, and others.
+         These books thus constitute a
+         <b>massive, distributed archive of the history of reading and book use,</b>
+         hidden in plain sight in the stacks of libraries. We want to find these books and make them available to researchers.
       </p>
       <p>
-         <router-link to="/">Book Traces</router-link> is a crowd-sourced web project aimed at identifying unique copies of
-         <b>nineteenth- and early twentieth-century books on library shelves.</b> Our focus is on customizations made by original owners
-         in personal copies, primarily in the form of marginalia, annotation, inscriptions, and insertions.
+         Book Traces is meant to engage
+         <b>the question of the future of the print record</b>
+         in the wake of wide-scale digitization. The issue is particularly urgent for the materials from the long nineteenth century.
+         In most cases, pre-1800 books have been moved to special collections, and post-1924 materials remain in copyright and thus on the
+         shelves for circulation. But academic libraries are now reconfiguring access to public-domain texts via digital repositories such as
+         Hathi Trust and Google Books. As a result, we are facing the
+         <a href="https://www.tandfonline.com/doi/full/10.1080/01930826.2015.1105668" target="_blank">withdrawal of many books</a>
+         in favor of digital surrogates and
+         <a href="https://www.oclc.org/research/publications/library/2013/2013-09r.html" target="_blank">
+         shared access to books across libraries</a>.
       </p>
       <p>
-        Sponsored by <a href="http://nines.org" target="_blank">NINES</a> 
-        at the <a href="http://virginia.edu" target="_blank">University of Virginia</a> and led by 
-        <a href="http://www.engl.virginia.edu/people/ams4k" target="_blank">Andrew Stauffer</a>, Book Traces is 
-        meant to engage <b>the question of the future of the print record</b> in the wake of wide-scale digitization. 
-        The issue is particularly urgent for the materials from the long nineteenth century. In most cases, 
-        pre-1800 books have been moved to special collections, and post-1923 materials remain in copyright 
-        and thus on the shelves for circulation. But academic libraries are now reconfiguring access to public-domain 
-        texts via repositories such as Hathi Trust and Google Books. The nineteenth-century books themselves aren’t 
-        getting much use. As a result, we are anticipating the withdrawal of large portions of the print collections in favor of 
-        digital surrogates and shared access to books across libraries.
-      </p>
-      <p>
-        We believe that, as libraries begin to manage down their print holdings, special attention 
-        should be paid to these historical materials, which in many cases came to university libraries 
-        from alumni donors and <b>bear marks of use by their original nineteenth-century owners.</b> 
-        These books thus constitute a <b>massive, distributed archive of the history of reading and book-use,</b> 
-        hidden in plain sight in the circulating collections. Marginalia, inscriptions, photos, original manuscripts, 
-        letters, drawings, and many other unique pieces of historical data can be found in individual copies, 
-        many of them associated with the history of the institution that collected the books in the first place. These 
-        unique attributes cannot be located by any electronic catalog. <span class="big">Each book has to be opened and examined.</span>
+         We believe that special attention should be paid to these historical materials, which <b>bear marks of use by their original owners.</b>
+         &nbsp;<router-link to="/submissions/1">Marginalia</router-link>, 
+         &nbsp;<router-link to="/submissions/361">inscriptions</router-link>, 
+         &nbsp;<router-link to="/submissions/69">photos</router-link>, 
+         &nbsp;<router-link to="/submissions/1233">original manuscripts</router-link>, 
+         &nbsp;<router-link to="/submissions/508">letters</router-link>, 
+         &nbsp;<router-link to="/submissions/578">drawings</router-link>, 
+         &nbsp;<router-link to="/submissions/397">locks of hair</router-link>, 
+         &nbsp;<router-link to="/submissions/1196">flowers</router-link>, and many other unique pieces
+         of historical data can be found in individual copies, many of them associated with the history of the institution that
+         collected the books in the first place. These unique attributes cannot be located by any electronic catalog.
+         <span class="big">Each book has to be opened and examined.</span>
       </p>
       <p>
         <span class="big">That’s where you come in.</span> Thousands of such books — marked or otherwise customized by nineteenth-century 
@@ -40,25 +59,76 @@
         for coordinating the management of nineteenth-century printed materials in academic libraries.
       </p>
       <p>
+         Book Traces is sponsored by <a href="http://nines.org" target="_blank">NINES</a> at
+         the <a href="http://virginia.edu" target="_blank">University of Virginia</a>, with 
+         <a href="https://news.library.virginia.edu/2017/06/01/hidden-in-plain-sight-book-traces-uva-a-clir-hidden-collections-project/" 
+            targte="_blank">funding from CLIR</a> and the 
+         <a href="https://www.mellon.org/grants/grants-database/grants/university-of-virginia/1901-06518/" target="_blank">Andrew W. Mellon Foundation</a>.
+      </p>
+      <p>
         If you have any questions or remarks, please contact <a href="mailto:ams4k@virginia.edu">Andrew Stauffer (ams4k@virginia.edu)</a> 
         or <a href="mailto:khj5c@virginia.edu">Kristin Jensen (khj5c@virginia.edu)</a>. 
-        And please, <b>share the site 
-        widely!</b> We want as many seekers out there in the library stacks as we can muster. The more examples 
-        we can locate, the more we will know about the history and future of the nineteenth century book.
+        And please, <b>share the site widely!</b> We want as many seekers out there in the library stacks as we can muster. 
+        The more examples we can locate, the more we will know about the history and future of the nineteenth-century book. We can also help co-ordinate 
+        class exercises based on stack searches in your library.
       </p>
-      <p><router-link to="/press">Want more information on Book Traces?</router-link></p>
+      <h4>Book Traces Scholarly Advisory Board</h4>
+      <ul>
+         <li>Thomas Augst (NYU)</li>
+         <li>Michael Cohen (UCLA)</li>
+         <li>Patricia Crain (NYU)</li>
+         <li>Steve Cushman (UVA)</li>
+         <li>Lindsey Eckert (Florida State)</li>
+         <li>Sarah Gardner (Mercer</li>
+         <li>Caroline Janney (UVA)</li>
+         <li>Mary Louise Kete (Vermont)</li>
+         <li>Amanda Licastro (Stevenson)</li>
+         <li>Devoney Looser (Arizona State)</li>
+         <li>Christina Lupton (Copenhagen)</li>
+         <li>Deborah Lutz (Louisville)</li>
+         <li>Deidre Lynch (Harvard)</li>
+         <li>Jerome McGann (UVA)</li>
+         <li>Elizabeth McHenry (NYU)</li>
+         <li>Jonathan Mulrooney (Holy Cross)</li>
+         <li>Andrew Piper (McGill)</li>
+         <li>Leah Price (Rutgers)</li>
+         <li>Jonathan Sachs (Concordia)</li>
+         <li>Steve Stowe (Indiana)</li>
+         <li>Mark Towsey (Liverpool)</li>
+         <li>Jon Wells (Michigan)</li>
+      </ul>
+      <h4>Partner Libraries</h4>
+      <ul>
+         <li>Arizona State University</li>
+         <li>Brandeis University</li>
+         <li>Bryn Mawr College</li>
+         <li>Louisiana State University</li>
+         <li>University of Connecticut</li>
+         <li>University of Miami</li>
+         <li>University of Virginia</li>
+      </ul>
    </div>
 </template>
 
 <script>
 export default {
    name: "about",
-   methods: {
-   }
+   methods: {}
 };
 </script>
 
 <style scoped>
+dl {
+   padding-bottom: 10px;
+   margin-bottom: 10px;
+   border-bottom: 1px dashed;
+}
+dt, dd {
+   margin-bottom: 10px;
+}
+dt {
+   font-weight: bold;
+}
 span.big {
    font-weight: bold;
    font-size: 1.1em;
--- go.mod
+++ go.mod
@@ -3,22 +3,24 @@ module github.com/uvalib/archive-submit
 go 1.12
 
 require (
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/contrib v0.0.0-20190923054218-35076c1b2bea
-	github.com/gin-gonic/gin v1.4.0
-	github.com/go-ozzo/ozzo-dbx v1.0.15
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-ozzo/ozzo-dbx v1.2.0
+	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/json-iterator/go v1.1.7 // indirect
-	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/rs/xid v1.2.1
 	github.com/stretchr/objx v0.2.0 // indirect
-	github.com/ugorji/go v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	golang.org/x/sys v0.0.0-20191002091554-b397fe3ad8ed // indirect
+	golang.org/x/sys v0.0.0-20191210023423-ac6580df4449 // indirect
 	golang.org/x/tools v0.0.0-20190708203411-c8855242db9c // indirect
-	google.golang.org/appengine v1.6.4 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.4 // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/go-playground/validator.v9 v9.30.2 // indirect
+	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
